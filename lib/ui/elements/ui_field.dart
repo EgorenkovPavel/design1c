3ef25656d2536347a08b2225295fc16e3831ebb1
Павел <epipasha@gmail.com>
2022-06-19 16:36:06 +0300

    Fixes

--- lib/ui/elements/ui_field.dart
+++ lib/ui/elements/ui_field.dart
@@ -1,20 +1,43 @@
 import 'package:design1c/data/elements/data_field.dart';
-import 'package:design1c/ui/resize_marker.dart';
-import 'package:design1c/ui/ui_element.dart';
 import 'package:design1c/utils/values.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class UIField extends StatelessWidget {
+import '../home_bloc.dart';
+
+class UIField extends StatefulWidget {
   final DataField data;
   final bool isActive;
-  final void Function(DataField newData)? onUpdate;
 
   const UIField({
     Key? key,
     required this.data,
     required this.isActive,
-    this.onUpdate,
   }) : super(key: key);
+
+  @override
+  State<UIField> createState() => _UIFieldState();
+}
+
+class _UIFieldState extends State<UIField> {
+
+  late DataField _data;
+
+  @override
+  void initState() {
+    super.initState();
+    _data = widget.data;
+  }
+
+  void updateWidths(BuildContext context, Offset offset) {
+    final delta = offset.dx;
+
+    _data = _data.copyWith(width: _data.width + delta);
+
+    setState(() {});
+
+    context.read<HomeBloc>().add(OnElementChanged(_data));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,26 +50,49 @@ class UIField extends StatelessWidget {
             borderRadius: const BorderRadius.all(Radius.circular(2.0)),
             border: Border.all(
                 color: FormColors.textFieldBorderColor,
-                width: isActive ? 2 : 1),
+                width: 1),
           ),
-          width: data.width,
+          width: _data.width,
           child: Align(
               alignment: Alignment.centerLeft,
               child: Text(
-                data.hint,
+                widget.data.hint,
                 style: TextStyles.textFieldTextStyle,
               )),
         ),
-        if (isActive)
-          ResizeMarker(
-              parentWidth: data.width,
-              parentHeight: Dimens.minRowHeight,
-              onUpdate: (newWidth) {
-                if (onUpdate != null) {
-                  onUpdate!(data.copyWith(width: newWidth));
-                }
-              })
+        if (widget.isActive)
+          Container(
+            width: _data.width,
+            height: Dimens.textFieldHeight,
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: Dimens.activeElementBorderWidth,
+                color: FormColors.activeElementBorderColor,
+              ),
+            ),
+          ),
+        if (widget.isActive)
+          Positioned(
+            top: Dimens.minRowHeight / 2 - _ballRadius,
+            right: 0 - _ballRadius,
+            child: GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onHorizontalDragUpdate: (details) {
+                updateWidths(context, details.delta);
+              },
+              child: Container(
+                height: _ballRadius * 2,
+                width: _ballRadius * 2,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: FormColors.activeElementBorderColor,
+                ),
+              ),
+            ),
+          )
       ],
     );
   }
 }
+
+const double _ballRadius = 5.0;
